bf59bb6fb558c4727e7248b86d0c5c878fa22a8f
alex-norman <alex.norman@coveainsurance.co.uk>
2020-05-13 23:09:51 +0100

    updated to speed up code

--- millionBankBalance/main.py
+++ millionBankBalance/main.py
@@ -27,12 +27,17 @@ def balance_at_million(deposit_1, deposit_2):
 
 
 max_days = 0
+first_dep = 100000000
+second_dep = 100000000
 
 for x in range(1, 1000000):
-    for y in range(1, 1000000):
-        million_balance = balance_at_million(x, y)
-        if(million_balance == 1000000):
+    if(x <= second_dep):
+        for y in range(x, 1000000):
             days_taken = days_to_million(x, y)
             if(days_taken > max_days):
-                max_days = days_taken
-                print(str(x) + " " + str(y) + " " + str(days_taken))
+                million_balance = balance_at_million(x, y)
+                if(million_balance == 1000000):
+                    first_dep = x
+                    second_dep = y
+                    max_days = days_taken
+                    print(str(x) + " " + str(y) + " " + str(days_taken))
